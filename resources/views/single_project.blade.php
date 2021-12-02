12c3500b90a22c38b5d3265d717df5a15bbce14f
Svetoslav Konakov <svetoslav.konakov@gmail.com>
2021-12-02 17:05:34 +0200

    Message form added to single_project

--- resources/views/single_project.blade.php
+++ resources/views/single_project.blade.php
@@ -37,5 +37,20 @@
 	    		</div>		    					    			    					    	 
 		    </tbody>
 	  </table>
+
+	  <form action = "/insert_message" method="post" class="form-group">
+	    <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>"><input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">	
+
+	    <label class="form-group">Project ID</label>
+	    <input type="text" name="project_id">
+	    
+	    <label>Author</label>
+	    <input type="text" name="author">
+
+	    <label>Content</label>
+	    <input type="text"  name="content">
+
+	    <button type="submit" value="Add project" >Submit</button>
+ 	</form>
 </body>
 </html>
